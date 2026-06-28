37fde4f5c04a59aa73b619b8a887fa4586217fb0
bossking567567-hash <bossking567567@gmail.com>
2026-06-28 16:22:27 +0500

    Fix swagger static files and add whitenoise storage

--- lms/settings.py
+++ lms/settings.py
@@ -106,3 +106,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
