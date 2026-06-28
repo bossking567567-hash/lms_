a50738170a10897a6551cc49af3189d4ddbc7062
bossking567567-hash <bossking567567@gmail.com>
2026-06-28 16:34:11 +0500

    Fix Render deployment

--- lms/urls.py
+++ lms/urls.py
@@ -1,21 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-from rest_framework import permissions
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 schema_view = get_schema_view(
     openapi.Info(
         title="LMS API",
-        default_version="v1",
-        description="LMS Project API Documentation",
+        default_version='v1',
+        description="LMS API",
     ),
     public=True,
-    permission_classes=[permissions.AllowAny],
+    permission_classes=(AllowAny,),
 )
 
 urlpatterns = [
@@ -25,11 +22,8 @@ urlpatterns = [
     path("api/", include("courses.urls")),
     path("api/", include("learning.urls")),
 
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/", TokenObtainPairView.as_view()),
+    path("api/token/refresh/", TokenRefreshView.as_view()),
 
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger"),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0)),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
