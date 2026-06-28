979def7e30faefefc2f0b190d9784273e3fedf0b
bossking567567-hash <bossking567567@gmail.com>
2026-06-28 18:15:22 +0500

    Fix Swagger error

--- courses/models.py
+++ courses/models.py
@@ -12,35 +12,9 @@ class Course(models.Model):
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    content = models.TextField()
-    video_url = models.URLField(blank=True, null=True)
-    order = models.IntegerField(default=0)
-    is_free = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
-
-from django.db import models
-from django.conf import settings
-
-
-class Course(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-
-
-class Lesson(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
-    title = models.CharField(max_length=255)
     content = models.TextField(blank=True, null=True)
-
-
     video_url = models.URLField(blank=True, null=True)
-
-
-    file = models.FileField(upload_to='lessons/', blank=True, null=True)
-
+    file = models.FileField(upload_to="lessons/", blank=True, null=True)
     order = models.IntegerField(default=0)
     is_free = models.BooleanField(default=False)
 
