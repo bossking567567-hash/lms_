fef7bfd3a74f67ba4531a0696cf1e46b6087f47e
bossking567567-hash <bossking567567@gmail.com>
2026-06-28 16:09:18 +0500

    Fix ALLOWED_HOSTS for Render

--- lms/settings.py
+++ lms/settings.py
@@ -1,27 +1,24 @@
 from pathlib import Path
 from datetime import timedelta
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = 'django-insecure-c9#up!=*m3z-f$3rpfslo!nn3lnrd4#+kf#so&%8)wcidg=ekt'
 
-
 DEBUG = False
 
-
+# Render silkangiz aniq ko'rsatildi
 ALLOWED_HOSTS = [
+    "lms-5-zgor.onrender.com",
     "lms-4-b5os.onrender.com",
     "localhost",
     "127.0.0.1",
 ]
 
-
 CSRF_TRUSTED_ORIGINS = [
+    "https://lms-5-zgor.onrender.com",
     "https://lms-4-b5os.onrender.com",
 ]
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -40,57 +37,37 @@ INSTALLED_APPS = [
     'learning',
 ]
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
-
     'django.middleware.common.CommonMiddleware',
-
     'django.middleware.csrf.CsrfViewMiddleware',
-
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-
     'django.contrib.messages.middleware.MessageMiddleware',
-
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
 ROOT_URLCONF = 'lms.urls'
-
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-
         'DIRS': [],
-
         'APP_DIRS': True,
-
         'OPTIONS': {
             'context_processors': [
-
                 'django.template.context_processors.request',
-
                 'django.contrib.auth.context_processors.auth',
-
                 'django.contrib.messages.context_processors.messages',
-
             ],
         },
     },
 ]
 
-
 WSGI_APPLICATION = 'lms.wsgi.application'
 
-
 AUTH_USER_MODEL = 'accounts.User'
-
 
 DATABASES = {
     'default': {
@@ -99,54 +76,29 @@ DATABASES = {
     }
 }
 
-
-
 REST_FRAMEWORK = {
-
     'DEFAULT_AUTHENTICATION_CLASSES': (
-
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-
     ),
-
 }
-
-
 
 SIMPLE_JWT = {
-
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
-
 }
-
-
 
 LANGUAGE_CODE = 'en-us'
 
-
 TIME_ZONE = 'UTC'
-
 
 USE_I18N = True
 
-
 USE_TZ = True
 
-
-
 STATIC_URL = '/static/'
-
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-
 MEDIA_URL = '/media/'
-
-
 MEDIA_ROOT = BASE_DIR / 'media'
-
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
